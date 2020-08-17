91a85764da63d856c578a22299ea818ab059e8cb
Mattias Andersson <neonithe@gmail.com>
2020-08-17 13:08:15 +0200

    Initial commit Udemy class

---
+++ src/test/java/com/udemy/example1/Spring/basic/SpringBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.example1.Spring.basic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
